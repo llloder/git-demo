d2d54f2935e550404a938577c2a648ff8c06d18a
llloder <2512541066@qq.com>
2022-01-02 20:30:39 +0800

    first commit

--- src/main/java/com/itcast/gitdemo1/GitDemo1.java
+++ src/main/java/com/itcast/gitdemo1/GitDemo1.java
@@ -3,5 +3,6 @@ package com.itcast.gitdemo1;
 public class GitDemo1 {
     public static void main(String[] args) {
         System.out.println("你好，Git！");
+        System.out.println("Nice To Meet To You");
     }
 }
