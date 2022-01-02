c2b8085153adb607fd018276949d8726329d821a
llloder <2512541066@qq.com>
2022-01-02 20:40:50 +0800

    hot-fix commit1

--- src/main/java/com/itcast/gitdemo1/GitDemo1.java
+++ src/main/java/com/itcast/gitdemo1/GitDemo1.java
@@ -4,5 +4,7 @@ public class GitDemo1 {
     public static void main(String[] args) {
         System.out.println("你好，Git！");
         System.out.println("Nice To Meet To You");
+        System.out.println("来聊个天吧！");
+
     }
 }
